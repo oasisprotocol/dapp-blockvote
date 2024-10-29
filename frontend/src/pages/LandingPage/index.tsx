3fd8dade9b79effec590904f9c2de548cc3fcaac
Kristof Csillag <kristof@oasisprotocol.org>
2024-10-29 11:05:18 +0100

    Decrease font size on landing page buttons

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -4,17 +4,13 @@ import { ConnectWallet } from '../../components/ConnectWallet'
 import classes from './index.module.css'
 import { Button, ButtonSize } from '../../components/Button'
 import { useNavigate } from 'react-router-dom'
-import { useAppState } from '../../hooks/useAppState'
 import { appName } from '../../constants/config'
 import { getPollPath } from '../../utils/path.utils'
 
 export const LandingPage: FC = () => {
   const navigate = useNavigate()
-  const {
-    state: { isMobileScreen },
-  } = useAppState()
   const openDemo = useCallback(() => navigate(getPollPath('demo')), [])
-  const buttonSize: ButtonSize = isMobileScreen ? 'small' : 'medium'
+  const buttonSize: ButtonSize = 'small'
 
   return (
     <Layout variation={'landing'}>
@@ -22,8 +18,8 @@ export const LandingPage: FC = () => {
         <h2>
           Welcome to <span className={'noWrap'}>{appName}</span>, a poll creation tool for your DAO.
         </h2>
-        To participate in a poll or create one, please connect your wallet above. This ensures secure and
-        verified interaction with the polling system.
+        To participate in a poll or create one, please connect your wallet. This ensures secure and verified
+        interaction with the polling system.
         <div className={'niceLineWide noWrap'}>
           <Button color={'secondary'} variant={'outline'} size={buttonSize} onClick={openDemo}>
             View Demo
