e40480121cbc51952d5b325d0e731e3dc233d51e
Kristof Csillag <kristof@oasisprotocol.org>
2025-07-04 03:48:05 +0200

    Forms: add support for hiding selects if there is only one option

--- frontend/src/components/InputFields/useOneOfField.ts
+++ frontend/src/components/InputFields/useOneOfField.ts
@@ -18,6 +18,7 @@ type OneOfFieldProps<DataType = string> = Omit<
   requiredMessage?: string
   hideDisabledChoices?: boolean
   disableIfOnlyOneVisibleChoice?: boolean
+  hideIfOnlyOneVisibleChoice?: boolean
 }
 
 export type OneOfFieldControls<DataType> = InputFieldControls<DataType> & {
@@ -30,6 +31,7 @@ export function useOneOfField<DataType>(props: OneOfFieldProps<DataType>): OneOf
     requiredMessage = 'Please select an option!',
     hideDisabledChoices,
     disableIfOnlyOneVisibleChoice,
+    hideIfOnlyOneVisibleChoice,
   } = props
   const visibleChoices = choices.filter(
     choice => !choice.hidden && (!hideDisabledChoices || getVerdict(choice.enabled, true)),
@@ -61,6 +63,7 @@ export function useOneOfField<DataType>(props: OneOfFieldProps<DataType>): OneOf
 
   return {
     ...controls,
+    visible: controls.visible && (visibleChoices.length > 1 || !hideIfOnlyOneVisibleChoice),
     choices,
   }
 }
