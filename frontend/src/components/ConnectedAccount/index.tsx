9670506e3bf5dd307f80dead63ac2add0cbdde1e
Kristof Csillag <kristof@oasisprotocol.org>
2025-07-12 00:35:43 +0200

    Show wallet dialog when connecting on address
    Offer "copy address", "view in explorer", "disconnect wallet"
    features, instead of just copying the address.

--- frontend/src/components/ConnectedAccount/index.tsx
+++ frontend/src/components/ConnectedAccount/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useCallback, useMemo, useState } from 'react'
 import { JazzIcon } from '../JazzIcon'
 import { useEthereum } from '../../hooks/useEthereum'
 import { StringUtils } from '../../utils/string.utils'
@@ -9,6 +9,18 @@ import { getChainDefinition } from '../../utils/poll.utils'
 import { getChainIconUrl } from '../../utils/crypto.demo'
 import { MotionDiv, shouldAnimate } from '../Animations'
 import { MaybeWithTooltip } from '../Tooltip/MaybeWithTooltip'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '../Dialog/dialog'
+import { Button } from '../Button'
+import CopyIcon from '@phosphor-icons/core/assets/regular/copy.svg?react'
+import SignOutIcon from '@phosphor-icons/core/assets/regular/sign-out.svg?react'
+import ExploreIcon from '@phosphor-icons/core/assets/regular/magnifying-glass.svg?react'
 
 interface Props {
   className?: string
@@ -17,23 +29,29 @@ interface Props {
 }
 
 export const ConnectedAccount: FC<Props> = ({ className, address, chainId }) => {
-  const { explorerBaseUrl } = useEthereum()
+  const [open, setOpen] = useState(false)
+  const handleClick = useCallback(() => setOpen(true), [])
+  const { explorerBaseUrl, disconnectWallet } = useEthereum()
   const {
     state: { isDesktopScreen },
   } = useAppState()
 
-  const chainDefinition = getChainDefinition(chainId)!
+  const chainDefinition = useMemo(() => getChainDefinition(chainId)!, [chainId])
 
-  const url = explorerBaseUrl ? StringUtils.getAccountUrl(explorerBaseUrl, address) : undefined
+  const url = useMemo(
+    () => (explorerBaseUrl ? StringUtils.getAccountUrl(explorerBaseUrl, address) : undefined),
+    [explorerBaseUrl, address],
+  )
   const imageUrl = getChainIconUrl(chainDefinition.icon)
 
-  return (
-    <a
-      href={url}
-      className={StringUtils.clsx(className, classes.connectedAccount)}
-      target="_blank"
-      rel="nofollow noreferrer"
-    >
+  const copyAddress = useCallback(() => void window.navigator.clipboard.writeText(address), [address])
+
+  const viewInExplorer = useCallback(() => {
+    if (url) window.open(url, '_blank')
+  }, [url])
+
+  const button = (
+    <a className={StringUtils.clsx(className, classes.connectedAccount)} onClick={handleClick}>
       {isDesktopScreen ? (
         <div className={classes.connectedAccountDetails}>
           {shouldAnimate('walletExtend') ? (
@@ -60,5 +78,43 @@ export const ConnectedAccount: FC<Props> = ({ className, address, chainId }) => 
         <JazzIcon size={20} address={address} />
       )}
     </a>
+  )
+
+  return (
+    <>
+      {button}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className={'text-center'}>
+              <JazzIcon size={60} address={address} />
+            </DialogTitle>
+            <DialogDescription className={'text-center'}>
+              <AddressShower address={address} className={classes.connectedAccountAddress} />
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button size={'small'} color={'secondary'} variant={'outline'} onClick={copyAddress}>
+              <div className={'flex flex-col items-center'}>
+                <CopyIcon fontSize={'1.5em'} />
+                Copy Address
+              </div>
+            </Button>
+            <Button size={'small'} color={'secondary'} variant={'outline'} onClick={viewInExplorer}>
+              <div className={'flex flex-col items-center'}>
+                <ExploreIcon fontSize={'1.5em'} />
+                View in Explorer
+              </div>
+            </Button>
+            <Button size={'small'} color={'secondary'} variant={'outline'} onClick={disconnectWallet}>
+              <div className={'flex flex-col items-center'}>
+                <SignOutIcon fontSize={'1.5em'} />
+                Disconnect
+              </div>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
